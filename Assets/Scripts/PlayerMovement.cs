5d40845b9dcbe2ee310187019f9181b2fa8d111b
fireteam1013 <emurabata@gmail.com>
2016-06-29 23:53:04 -0700

    Update to ThirdPersonController

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour {
     private GameObject cam;
     private GameObject camPivot;
 
-    private ThirdPersonCharacter m_Character;   //Get class
+    //private ThirdPersonCharacter m_Character;   //Get class
     private Transform m_Cam;                    //Transform of the main camera
     private Vector3 m_CamForward;               //Direction facing forward
     private Vector3 m_Move;                     //Movement
@@ -20,7 +20,7 @@ public class PlayerMovement : MonoBehaviour {
     void Start()
     {
         cam = GameObject.Find("Main Camera");
-        camPivot = GameObject.Find("PlayerCamPivot");
+        camPivot = GameObject.Find("CameraPivot");
         rb = GetComponent<Rigidbody>();
         if (Camera.main != null)
         {
@@ -31,7 +31,8 @@ public class PlayerMovement : MonoBehaviour {
             Debug.LogWarning("Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
             // we use self-relative controls in this case, which probably isn't what the user wants, but hey, we warned them!
         }
-        m_Character = GetComponent<ThirdPersonCharacter>();
+        //m_Character = GetComponent<ThirdPersonCharacter>();
+        cam.transform.parent = camPivot.transform;
     }
 
     void Update()
