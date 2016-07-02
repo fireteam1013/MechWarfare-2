e665e7f5afb945339b365d985642006c9ecb5f88
fireteam1013 <emurabata@gmail.com>
2016-07-01 23:38:48 -0700

    Inventory Update
    Started work on inventory, armor and health.

---
+++ Assets/Scripts/TestDamage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestDamage : MonoBehaviour {
+
+    private float attack = 50f;
+    public GameObject target;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            target.SendMessage("Calc_Damage", attack, SendMessageOptions.DontRequireReceiver);
+            
+        }
+    }
+}
