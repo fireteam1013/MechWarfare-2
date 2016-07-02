77d44d541ca4e9f69658a5d83bc332c86bff7a09
fireteam1013 <emurabata@gmail.com>
2016-07-02 00:33:33 -0700

    rough update

---
+++ Assets/Scripts/ThirdPersonInput.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonInput : MonoBehaviour {
+
+    private PlayerMovement p_Movement;
+    private Transform p_Cam;
+    private Vector3 p_CamForward;
+    private Vector3 p_Move;
+    private bool p_Jump;
+
+
+    private void Start()
+    {
+        p_Movement = GetComponent<PlayerMovement>();
+    }
+}
